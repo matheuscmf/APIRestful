8a38a6b39090c6038325110171871dedaff5f798
Matheus Marques Albuquerque <matheuscmf1995@gmail.com>
2024-04-03 20:21:07 -0300

    Client Service and Repository

---
+++ backend/src/main/java/com/unifametro/apirestful/services/ClientService.java
@@ -0,0 +1,23 @@
+package com.unifametro.apirestful.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unifametro.apirestful.entities.Client;
+import com.unifametro.apirestful.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Client> findAll(){
+		
+		return repository.findAll();
+	}
+}
