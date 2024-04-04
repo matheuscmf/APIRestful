adcbf958ff957aa4fc080646f8bddec24942b0e3
Matheus Marques Albuquerque <matheuscmf1995@gmail.com>
2024-04-03 22:08:05 -0300

    Seeding inserted

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Auricelio Freitas', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1982-08-28T10:30:00Z', 10700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Miguel Soares', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1989-02-10T10:30:00Z', 20700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Matheus Marques', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1992-11-23T10:30:00Z', 10700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Rafael Sales', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1988-10-13T10:30:00Z', 10700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Bruno Freitas', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1987-04-22T10:30:00Z', 9700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Romulo Araujo', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1991-01-09T10:30:00Z', 8700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('Ryan Sousa', '000.111.222-33', TIMESTAMP WITH TIME ZONE '1994-03-26T10:30:00Z', 7700.5);
+INSERT INTO tb_client (name, cpf, birth_date, income) VALUES ('test1', '000.111.222-33', null, 1024.0);
