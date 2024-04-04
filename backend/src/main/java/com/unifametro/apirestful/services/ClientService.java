5367de4a3165f8dcba89fd583c847e2c2a0a5b54
Matheus Marques Albuquerque <matheuscmf1995@gmail.com>
2024-04-03 23:20:48 -0300

    Created DTO

--- backend/src/main/java/com/unifametro/apirestful/services/ClientService.java
+++ backend/src/main/java/com/unifametro/apirestful/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.unifametro.apirestful.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.unifametro.apirestful.dto.ClientDTO;
 import com.unifametro.apirestful.entities.Client;
 import com.unifametro.apirestful.repositories.ClientRepository;
 
@@ -16,8 +18,9 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
+	public List<ClientDTO> findAll(){
 		
-		return repository.findAll();
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
